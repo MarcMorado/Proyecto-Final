cdc43a8639072097de10d18548c21e64a5f4e2d1
MarcMorado <marcmp96@gmail.com>
2023-03-29 21:12:15 +0200

    update modal

--- src/components/CharacterSheet.jsx
+++ src/components/CharacterSheet.jsx
@@ -1,15 +1,103 @@
 import { CharacterContext } from "../context/CharacterContext";
 import { useContext } from "react";
-import '../styles/StylesCharacter.css'
+import imgBg from "../assets/sheet/asdddd26.png";
+import imgStat from "../assets/sheet/stat.png";
+import "../styles/StylesCharacter.css";
 
 export default function CharacterSheet() {
   const { selectedCharacter, openSheet } = useContext(CharacterContext);
 
   return (
     openSheet && (
-      <div>
-        <div className="sheet-modal">
-          <p>{selectedCharacter.charName}</p>
+      <div className="bg-sht">
+        <div className="sht-modal">
+          <div className="sht-container">
+            <img src={imgBg} alt="bg" />
+          </div>
+          <div className="sht-stats">
+            <div className="sht-stat">
+              <div className="container">
+                <div className="content">
+                  <p className="text">str</p>
+                  <p className="stat-number">{selectedCharacter.stats.str}</p>
+                  <div className="plus-minus">
+                    <button className="btn-minus">-</button>
+                    <p>{Math.floor((selectedCharacter.stats.str - 10) / 2)}</p>
+                    <button className="btn-plus">+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="sht-stat">
+              <div className="container">
+                <div className="content">
+                  <p className="text">dex</p>
+                  <p className="stat-number">{selectedCharacter.stats.dex}</p>
+                  <div className="plus-minus">
+                    <button className="btn-minus">-</button>
+                    <p>{Math.floor((selectedCharacter.stats.dex - 10) / 2)}</p>
+                    <button className="btn-plus">+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="sht-stat">
+              <div className="container">
+                <div className="content">
+                  <p className="text">cons</p>
+                  <p className="stat-number">{selectedCharacter.stats.const}</p>
+                  <div className="plus-minus">
+                    <button className="btn-minus">-</button>
+                    <p>{Math.floor((selectedCharacter.stats.const - 10) / 2)}</p>
+                    <button className="btn-plus">+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="sht-stat">
+              <div className="container">
+                <div className="content">
+                  <p className="text">int</p>
+                  <p className="stat-number">{selectedCharacter.stats.int}</p>
+                  <div className="plus-minus">
+                    <button className="btn-minus">-</button>
+                    <p>{Math.floor((selectedCharacter.stats.int - 10) / 2)}</p>
+                    <button className="btn-plus">+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="sht-stat">
+              <div className="container">
+                <div className="content">
+                  <p className="text">wis</p>
+                  <p className="stat-number">{selectedCharacter.stats.wis}</p>
+                  <div className="plus-minus">
+                    <button className="btn-minus">-</button>
+                    <p>{Math.floor((selectedCharacter.stats.wis - 10) / 2)}</p>
+                    <button className="btn-plus">+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="sht-stat">
+              <div className="container">
+                <div className="content">
+                  <p className="text">char</p>
+                  <p className="stat-number">{selectedCharacter.stats.char}</p>
+                  <div className="plus-minus">
+                    <button className="btn-minus">-</button>
+                    <p>{Math.floor((selectedCharacter.stats.char - 10) / 2)}</p>
+                    <button className="btn-plus">+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import { CreateGameProvider } from "./context/CreateGameContext";
 import "./fonts/NotoSansJP-Black.otf";
 import "./fonts/NotoSansJP-Bold.otf";
 import "./fonts/NotoSansJP-Light.otf";
 import "./fonts/NotoSansJP-Medium.otf";
 import "./fonts/NotoSansJP-Regular.otf";
 import "./fonts/NotoSansJP-Thin.otf";
+import "./fonts/Diablo/Diablo-Light.ttf";
+import "./fonts/Diablo/Diablo-Heavy.ttf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <CreateGameProvider> */}
         <App />
-      {/* </CreateGameProvider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
